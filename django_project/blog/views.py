02ad8c74ae60cf7b49c7260a74649082aeb46116
Alex Scarlett <alex.scarlett91@gmail.com>
2019-10-07 22:13:14 -0400

    Set up Admin Page, Database, and Initial Migration

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,25 +1,10 @@
 from django.shortcuts import render
-
-
-posts = [
-    {
-        'author': 'AlexS',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'October 1, 2019'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'September 29, 2019'
-    }
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
